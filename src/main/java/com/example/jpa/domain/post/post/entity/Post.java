e9a1828ddea7b01e6a909bbe00a6f8e964a217bb
kozae00 <rhwogus777@gmail.com>
2025-01-20 11:44:56 +0900

    jpa auditing apply

--- src/main/java/com/example/jpa/domain/post/post/entity/Post.java
+++ src/main/java/com/example/jpa/domain/post/post/entity/Post.java
@@ -1,28 +1,36 @@
 package com.example.jpa.domain.post.post.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-// @Setter // Setter는 사용하지 않는 것이 좋다. Setter를 사용하면 객체의 불변성이 깨지기 때문
 @Entity
 @Getter
 @AllArgsConstructor // 생성자
 @NoArgsConstructor // 기본 생성자
 @Builder // 빌더 패턴을 사용
-// AllArgsConstructor, NoArgsConstructor, Builder를 사용하면 생성자를 직접 만들지 않아도 된다. 3개는 항상 같이 묶여야 함.
+@EntityListeners(AuditingEntityListener.class)
+@Setter
 
 public class Post {
 
     @Id // Primary Key
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto Increment
+    @Setter(AccessLevel.PRIVATE)
     private Long id; // long -> null X, Long  -> null O : JPA 입장에서는 null이 가능해야 하므로 원시 -> 객체타입으로 변경
+
+    @CreatedDate
+    @Setter(AccessLevel.PRIVATE)
     private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Setter(AccessLevel.PRIVATE)
     private LocalDateTime modifiedDate;
+
     @Column(length = 100)
     private String title;
     @Column(columnDefinition = "TEXT")
--- src/main/java/com/example/jpa/domain/post/post/service/PostService.java
+++ src/main/java/com/example/jpa/domain/post/post/service/PostService.java
@@ -5,8 +5,6 @@ import com.example.jpa.domain.post.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-
 @Service
 @RequiredArgsConstructor
 public class PostService {
@@ -14,28 +12,11 @@ public class PostService {
     private final PostRepository postRepository;;
     public Post write(String title, String body) {
         // Post 조립
-
         Post post = Post.builder()
                 .title(title)
                 .body(body)
-                .createdDate(LocalDateTime.now())
-                .modifiedDate(LocalDateTime.now())
-                .id(null)
                 .build();
 
-//        Post post = new Post(
-//                null,
-//                LocalDateTime.now(),
-//                LocalDateTime.now(),
-//                title,
-//                body
-//        );
-
-        // post.setId(1L); id는 기본적으로 JPA가 관리. 직접 입력 X
-//        post.setCreatedDate(LocalDateTime.now());
-//        post.setModifiedDate(LocalDateTime.now());
-//        post.setTitle(title);
-//        post.setBody(body);
 
         // repository 넘김
         return postRepository.save(post);
