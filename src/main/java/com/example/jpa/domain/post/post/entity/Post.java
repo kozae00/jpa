8fd3596a6f8ddeb5d8967a7e4fb019afe7d06854
kozae00 <rhwogus777@gmail.com>
2025-01-20 10:41:09 +0900

    BaseInitData

--- src/main/java/com/example/jpa/domain/post/post/entity/Post.java
+++ src/main/java/com/example/jpa/domain/post/post/entity/Post.java
@@ -1,15 +1,19 @@
 package com.example.jpa.domain.post.post.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Setter
+@Getter
 public class Post {
 
     @Id // Primary Key
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto Increment
-    private long id;
+    private Long id; // long -> null X, Long  -> null O : JPA 입장에서는 null이 가능해야 하므로 원시 -> 객체타입으로 변경
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
     @Column(length = 100)
---
+++ src/main/java/com/example/jpa/domain/post/post/service/PostService.java
@@ -0,0 +1,30 @@
+package com.example.jpa.domain.post.post.service;
+
+import com.example.jpa.domain.post.post.entity.Post;
+import com.example.jpa.domain.post.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;;
+    public Post write(String title, String body) {
+        // Post 조립
+        Post post = new Post();
+
+        // post.setId(1L); id는 기본적으로 JPA가 관리. 직접 입력 X
+        post.setCreatedDate(LocalDateTime.now());
+        post.setModifiedDate(LocalDateTime.now());
+        post.setTitle(title);
+        post.setBody(body);
+
+        // repository 넘김
+        postRepository.save(post);
+
+        return post;
+    }
+}
