99400e382e88102c304110213895c459f3bfbc59
kozae00 <rhwogus777@gmail.com>
2025-01-20 10:58:19 +0900

    count

--- src/main/java/com/example/jpa/global/BaseInitData.java
+++ src/main/java/com/example/jpa/global/BaseInitData.java
@@ -16,12 +16,17 @@ public class BaseInitData {
     @Bean
     public ApplicationRunner applicationRunner() {
         return args -> {
+
+           if(postService.count() > 0) {
+               return;
+           }
+
            Post p1 = postService.write("title1", "body1");
            System.out.println(p1.getId() + "번 포스트가 생성되었습니다.");
            Post p2 = postService.write("title2", "body2");
-            System.out.println(p2.getId() + "번 포스트가 생성되었습니다.");
+           System.out.println(p2.getId() + "번 포스트가 생성되었습니다.");
            Post p3 = postService.write("title3", "body3");
-            System.out.println(p3.getId() + "번 포스트가 생성되었습니다.");
+           System.out.println(p3.getId() + "번 포스트가 생성되었습니다.");
         };
     }
 }
